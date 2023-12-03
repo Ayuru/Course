df4be06c44df12053c883c1641a82d597b11fa44
Ayu <ayuru.gi@gmail.com>
2023-12-04 00:06:53 +0100

    14.1 & 14.2

---
+++ patterns/src/main/java/com/example/patterns/prototype/library/Library.java
@@ -0,0 +1,32 @@
+package com.example.patterns.prototype.library;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Library extends Prototype<Library> {
+
+    final String name;
+    Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = super.clone();
+        clonedLibrary.books = new HashSet<>(books);
+        return clonedLibrary;
+    }
+}
